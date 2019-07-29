17044cfe3d43920406575deca1bf244289e38d6e
AZureDracula <43308880+AZureDracula@users.noreply.github.com>
2019-07-29 12:06:59 +0800

    轮播

--- sky31zx/js5/pageFive.js
+++ sky31zx/js5/pageFive.js
@@ -4,6 +4,7 @@ let ticking = false;
 function doSomething(leftS) {
 	document.querySelector('#Four_ship').style.transform = 'translateX(' + leftS * 0.25 + "px)";
 };
+
 document.querySelector("#pageFive_2").onscroll = () => {
 	leftS = document.querySelector("#pageFive_2").scrollLeft
 	if(!ticking) {
