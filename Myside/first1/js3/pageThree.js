350f4bf3a3c9840e30e0ca54ecf460efc37f459f
AZureDracula <43308880+AZureDracula@users.noreply.github.com>
2019-08-05 16:09:36 +0800

    das

--- Myside/first1/js3/pageThree.js
+++ Myside/first1/js3/pageThree.js
@@ -3,10 +3,10 @@ $(function(){
         var aa = $(this).scrollLeft()
         console.log("混动条1位置"+aa)
         if(aa > 150){
-            document.querySelector('.texttwo').style.display = 'block';
+            document.querySelector('.Thrid_texttwo').style.display = 'block';
         }
         if(aa > 440){
-            document.querySelector('.textthree').style.display = 'block';
+            document.querySelector('.Thrid_textthree').style.display = 'block';
         }
     })
 })
