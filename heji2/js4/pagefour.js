478e3ae08d4c0c84818b3c2fc6642bdf7cd1178c
AZureDracula <43308880+AZureDracula@users.noreply.github.com>
2019-08-09 21:55:57 +0800

    asda

---
+++ heji2/js4/pagefour.js
@@ -0,0 +1,16 @@
+
+$(function(){
+    $(".pagefour").scroll(function(){
+        var bb = $(this).scrollLeft()
+//      console.log("混动条2位置"+bb);
+        if(bb >10){
+            document.querySelector('#four_lefthand').style.display = 'none';
+        }
+        if(bb > 120){
+            document.querySelector('.four_texttwo').style.display = 'block';
+        }
+        if(bb > 400){
+            document.querySelector('.four_textthree').style.display = 'block';
+        }
+    })
+})
